f10dccebdc7f5022e7414898718024c25c8d7d17
felipebns <131299435+felipebns@users.noreply.github.com>
2024-04-18 19:20:30 -0300

    meu trabalho

--- FRST- Aula 1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r'C:\Users\Arthu\Documents\dados_brutos.csv', sep=';')
-df_userPorOrg = df.groupby('organization_id')['user'].nunique().reset_index()
-df_userPorOrg.columns = ['organization_id', 'N_users']
-df_userPorOrg = df_userPorOrg.sort_values(by='N_users', ascending=False)
-top_20_orgs = df_userPorOrg.head(20)
-print(top_20_orgs)
-
-print('nao gostei')
