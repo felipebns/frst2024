462c3e640538829bdbb5766b5653fec279c0cae7
Thomas Lipsky <63546936+THOM472@users.noreply.github.com>
2024-04-29 15:57:17 -0300

    Update capacitação #3.py

--- capacitação #3.py
+++ capacitação #3.py
@@ -7,11 +7,13 @@ n = 1000
 media = 173
 sd = 7.3
 
-populations = np.random.normal(loc=media, scale=sd, size=(n, 10000)).flatten()
+populations = np.random.normal(loc=media, scale=sd, size=(n,1000)).flatten()
+
 
 media_amostral = np.mean(populations)
-inf = media - 1.96 * (sd / np.sqrt(n))
-sup = media + 1.96 * (sd / np.sqrt(n))
+
+inf = np.quantile(populations, 0.025)
+sup = np.quantile(populations, 0.975)
 
 sns.histplot(populations, kde=True, color='blue')
 plt.axvline(inf, color = 'red', label = 'Limite Inferior')
@@ -19,4 +21,3 @@ plt.axvline(sup, color = 'red', label = 'Limite Superior')
 plt.axvline(media_amostral, color='green', linestyle='--', label = 'média')
 plt.legend()
 plt.show()
-
